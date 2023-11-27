3bf04797c4e38ae9bc3ff660ab4530b2554ad501
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-11-27 15:44:13 -0700

    Update download-images.py

--- download-images.py
+++ download-images.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import requests
 import os
+import zipfile
+from io import BytesIO
 
-def download_image(url):
+def download_image(url, zipf):
     if url:
         try:
             response = requests.get(url)
@@ -10,9 +12,8 @@ def download_image(url):
                 # Extract image name from URL or generate a name
                 image_name = url.split("/")[-1] or f"image_{len(url_list)}.jpg"
                 
-                # Save the image
-                with open(image_name, 'wb') as file:
-                    file.write(response.content)
+                # Write the image to the zip file
+                zipf.writestr(image_name, response.content)
                 return True
         except Exception as e:
             st.error(f"Error downloading {url}: {e}")
@@ -27,9 +28,23 @@ urls = st.text_area("Enter the Image URLs (one per line)")
 if st.button('Download Images'):
     url_list = urls.split('\n')
     success_count = 0
-    for url in url_list:
-        if download_image(url):
-            success_count += 1
+
+    # Create a bytes buffer for the zip file
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zipf:
+        for url in url_list:
+            if download_image(url, zipf):
+                success_count += 1
+
+    # Set the pointer of the buffer to the beginning
+    zip_buffer.seek(0)
+
     st.success(f'Download Complete: {success_count}/{len(url_list)} images downloaded successfully.')
 
-# Remove the if __name__ == "__main__": main() part
+    # Provide a link to download the zip file
+    st.download_button(
+        label="Download Images as Zip",
+        data=zip_buffer,
+        file_name="downloaded_images.zip",
+        mime="application/zip"
+    )
