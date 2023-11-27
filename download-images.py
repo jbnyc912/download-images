980e79eef9158307d97f31ca39fdf8bf9ae4070c
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-11-27 15:41:14 -0700

    Update download-images.py

--- download-images.py
+++ download-images.py
@@ -1,10 +1,23 @@
-
 import streamlit as st
 import requests
+import os
 
 def download_image(url):
-    # Add logic to download and save the image
-    pass
+    if url:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                # Extract image name from URL or generate a name
+                image_name = url.split("/")[-1] or f"image_{len(url_list)}.jpg"
+                
+                # Save the image
+                with open(image_name, 'wb') as file:
+                    file.write(response.content)
+                return True
+        except Exception as e:
+            st.error(f"Error downloading {url}: {e}")
+            return False
+    return False
 
 st.title('Bulk Image Downloader')
 
@@ -13,6 +26,10 @@ urls = st.text_area("Enter the Image URLs (one per line)")
 
 if st.button('Download Images'):
     url_list = urls.split('\n')
+    success_count = 0
     for url in url_list:
-        download_image(url)
-    st.success('Download Complete')
+        if download_image(url):
+            success_count += 1
+    st.success(f'Download Complete: {success_count}/{len(url_list)} images downloaded successfully.')
+
+# Remove the if __name__ == "__main__": main() part
